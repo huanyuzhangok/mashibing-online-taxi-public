cf0ee97becc056a0bc7c94f6884c2bd458c9af1d
huanyuzhang <949219270@qq.com>
2023-10-09 17:45:12 +0800

    token 拦截器测试

---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/interceptor/InterceptorConfig.java
@@ -0,0 +1,24 @@
+package com.mashibing.apipassenger.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @className: InterceptorConfig
+ * @description: TODO 类描述
+ * @author: huanyuzhang
+ * @date: 2023/10/9
+ **/
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( new JwtInterceptor())
+                // 拦截的路径
+                .addPathPatterns("/**")
+                // 不拦截的路径
+                .excludePathPatterns("/noAuthTest");
+    }
+}
