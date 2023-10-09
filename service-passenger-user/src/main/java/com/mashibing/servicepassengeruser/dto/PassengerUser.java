248f9f8675428744ae1e5b368e3e6274930a5572
huanyuzhang <949219270@qq.com>
2023-10-09 15:29:17 +0800

    插入用户功能

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/dto/PassengerUser.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/dto/PassengerUser.java
@@ -3,6 +3,7 @@ package com.mashibing.servicepassengeruser.dto;
 import lombok.Data;
 import org.apache.tomcat.jni.Local;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 /**
@@ -14,8 +15,8 @@ import java.time.LocalTime;
 @Data
 public class PassengerUser {
     private long id;
-    private LocalTime gmtCreate;
-    private LocalTime gmtModified;
+    private LocalDateTime gmtCreate;
+    private LocalDateTime gmtModified;
     private String passengerPhone;
     private String passengerName;
     private byte passengerGender;
--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/impl/UserServiceImpl.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/impl/UserServiceImpl.java
@@ -7,9 +7,11 @@ import com.mashibing.servicepassengeruser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @className: UserServiceImpl
@@ -22,6 +24,7 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private PassengerUserMapper passengerUserMapper;
+
     @Override
     public ResponseResult loginOrRegister(String passengerPhone) {
         System.out.println("调用手机号" + passengerPhone);
@@ -29,10 +32,22 @@ public class UserServiceImpl implements UserService {
         Map<String, Object> map = new HashMap<>();
         map.put("passenger_phone", passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers == null || passengerUsers.size() == 0 ? "无记录" : passengerUsers.get(0).getPassengerPhone() );
         // 判断用户信息是否存在
+        if (passengerUsers.size() == 0) {
+            // 如果不存在插入用户信息
+            PassengerUser passengerUser = new PassengerUser();
+            // 生成随机的字符串
+            passengerUser.setPassengerName(UUID.randomUUID().toString().replace("-", "").substring(0, 10));
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte) 0);
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+            passengerUserMapper.insert(passengerUser);
+        }
 
-        // 如果不存在插入用户信息
+
         return ResponseResult.success();
     }
 }
