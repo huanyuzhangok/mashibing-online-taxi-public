458c01389f6c0ace4d91f870a7221728646eac87
huanyuzhang <949219270@qq.com>
2023-10-09 15:37:14 +0800

    service-passenger-user注册到nacos

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
@@ -3,6 +3,7 @@ package com.mashibing.servicepassengeruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.mashibing.servicepassengeruser.mapper")
 public class ServicePassengerUserApplication {
     public static void main(String[] args) {
